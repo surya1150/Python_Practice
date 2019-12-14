fb1129ddec625aff8ac86e37ecf519f43ca29d90
surya1150 <kotha.surya1150@gmail.com>
2019-12-15 00:46:28 +0530

    practice

--- minmax.py
+++ minmax.py
@@ -1,15 +1,1 @@
-import boto3
-
-# "region_name" here may be different depending on your default region
-client = boto3.client('ssm', region_name='us-east-1')
-s3 = boto3.client('s3')
-
-response = client.get_parameters(
-   Names=['UserName'],
-   WithDecryption=True
-)
-Key_Value = "key_value.txt"
-f = open("key_value.txt", "w")
-f.write('%s:%s\n' %('UserName', response['Parameters'][0]['Value']))
-f.close()
-response = s3.upload_file(Key_Value, "testdxc", Key_Value)
+print ("Test")
